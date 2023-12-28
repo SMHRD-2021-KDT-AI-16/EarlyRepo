e47d78ebda229fa15b026b662a1538588761f7b2
user <user@192.168.219.109>
2023-12-28 09:26:43 +0900

    박세명 지도 검색기능 커밋

--- GitTest1/src/main/java/com/early/db/ApartDAO.java
+++ GitTest1/src/main/java/com/early/db/ApartDAO.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.early.model.ApartVO;
+import com.early.model.SearchApartVO;
 
 
 
@@ -15,15 +16,26 @@ public class ApartDAO {
 	public List<ApartVO> getApart(String name) {
 		
 		SqlSession sqlSession = factory.openSession(true);
-		System.out.println("이름 들어옴?"+name);
+		//System.out.println("이름 들어옴?"+name);
 		
 		List<ApartVO> list = sqlSession.selectList("com.early.db.mapMapper.getApart", name);
 		//ApartVO test = sqlSession.selectOne("com.early.db.mapMapper.getApart2", name);
-		System.out.println("여기까지 들어옴22?");
-		System.out.println("list : "+list.get(0).getApt_loc());
+		//System.out.println("여기까지 들어옴22?");
+		//System.out.println("list : "+list.get(0).getApt_loc());
 		//System.out.println("test : "+test);
 		
 		sqlSession.close();
+		return list;
+	}
+	
+	public List<SearchApartVO> getApartSearch(String name){
+		SqlSession sqlSession = factory.openSession(true);
+		List<SearchApartVO> list = sqlSession.selectList("com.early.db.mapMapper.getApartSearch", name);
+		
+			System.out.println("주소값 : "+list.get(0).getApt_name());
+		
+		sqlSession.close();
+		
 		return list;
 	}
 }
